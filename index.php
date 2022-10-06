175894e2208f2dc49b59dda9daa633ed06617653
Jose Urbina Najera <jose.urbinanajera@uhn.ca>
2022-10-06 15:58:35 -0400

    Added favicon

--- index.php
+++ index.php
@@ -90,6 +90,7 @@
            <head>
               <meta charset="UTF-8">
               <title>mCODER</title>
+              <link rel="icon" type="image/png" href="logo.png" />
               <link rel="stylesheet" type="text/css" href="css/modal.css">
               <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
               <script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
