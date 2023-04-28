be8bf0a87d7189470883ce26a6a10063930b9f54
jurbinan <jurbinan@gmail.com>
2023-04-28 10:04:05 -0400

    Added PDO support to the database config files and documented config files

--- configuration/mcode.php
+++ configuration/mcode.php
@@ -1,4 +1,11 @@
 <?php
-$mysqli = new mysqli("","","","");
-$connect = mysqli_connect("","","","");
-?>
+//parameters: host, username, password, db
+$mysqli = new mysqli("", "", "", "");
+//parameters: host, username, password, db
+$connect = mysqli_connect("", "", "", "");
+
+try {
+    $mcode_db_pdo = new PDO("mysql:host=;dbname=", "", "");
+} catch (PDOException $e) {
+    echo "Connection error: " . $e->getMessage();
+}
--- configuration/server.php
+++ configuration/server.php
@@ -1,18 +1,18 @@
 <?php
 
 $provider = new Stevenmaguire\OAuth2\Client\Provider\Keycloak([
+  // the keycloak server's root url and port. Must include protocol (http or https)
   'authServerUrl'             => '',
-  'realm'                     => '',
-  'clientId'                  => '',
-  'clientSecret'              => '',
-  'redirectUri'               => '',
+  'realm'                     => '', // the realm's name
+  'clientId'                  => '', // the client ID for the app in keycloak
+  'clientSecret'              => '', // the client secret (available in keycloak)
+  'redirectUri'               => '', // the URL to redirect to after a successful login
   'encryptionAlgorithm'       => null,
   'encryptionKey'             => null,
   'encryptionKeyPath'         => null
 ]);
 
 $clientID = "";
-$KEYCLOAKDOMAIN = "";
-$MCODERDOMAIN = "";
+$KEYCLOAKDOMAIN = ""; // must include the protocol (http or https)
+$MCODERDOMAIN = ""; // must include the protocol (http or https)
 $REALM = "";
-?>
